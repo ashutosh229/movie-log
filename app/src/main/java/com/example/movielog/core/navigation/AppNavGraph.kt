5c8b8a836c3631a6de2ca1b34a1fe364c39867f9
ashutosh229 <akumarjha875@gmail.com>
2026-03-30 12:22:50 +0530

    fix: resolve the signup page crash by adding it in the navigation graph

--- app/src/main/java/com/example/movielog/core/navigation/AppNavGraph.kt
+++ app/src/main/java/com/example/movielog/core/navigation/AppNavGraph.kt
@@ -1,11 +1,13 @@
 package com.example.movielog.core.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.NavHost;
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.movielog.features.auth.presentation.screen.LoginScreen
-import com.example.movielog.features.auth.presentation.viewmodel.AuthViewModel
+import com.example.movielog.features.auth.presentation.screen.SignupScreen
+import com.example.movielog.features.auth.presentation.viewmodel.AuthViewModel;
+import com.example.movielog.features.library.presentation.screen.HomeScreen
 
 @Composable
 fun AppNavGraph(viewModel: AuthViewModel) {
@@ -15,20 +17,37 @@ fun AppNavGraph(viewModel: AuthViewModel) {
     } else {
         Routes.LOGIN
     }
-    NavHost(
-        navController = navController, startDestination = startDestination
-    ) {
+    NavHost(navController = navController, startDestination = startDestination) {
         composable(Routes.LOGIN) {
-            LoginScreen(viewModel = viewModel, onSwitchToSignup = {
-                navController.navigate(Routes.SIGNUP)
-            }, onLoginSuccess = {
-                navController.navigate(Routes.HOME) {
-                    popUpTo(Routes.LOGIN) {
-                        inclusive = true
+            LoginScreen(
+                viewModel = viewModel,
+                onSwitchToSignup = { navController.navigate(Routes.SIGNUP) },
+                onLoginSuccess = {
+                    navController.navigate(Routes.HOME) {
+                        popUpTo(Routes.LOGIN) {
+                            inclusive = true
+                        }
                     }
-                }
-            }
-            )
+                })
+        }
+        composable(Routes.SIGNUP) {
+            SignupScreen(
+                viewModel = viewModel,
+                onSwitchToLogin = { navController.navigate(Routes.SIGNUP) },
+                onSignupSuccess = {
+                    navController.navigate(Routes.HOME) {
+                        popUpTo(Routes.SIGNUP) {
+                            inclusive = true
+                        }
+                    }
+                })
+        }
+        composable(Routes.HOME) {
+            HomeScreen(onLogout = {
+                viewModel.logout();navController.navigate(
+                Routes.LOGIN
+            ) { popUpTo(Routes.HOME) { inclusive = true } }
+            })
         }
     }
 }
