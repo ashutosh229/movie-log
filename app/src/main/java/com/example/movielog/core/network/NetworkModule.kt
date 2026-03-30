65f813553f25a78cf6a326fdaa2d285750874a4e
ashutosh229 <akumarjha875@gmail.com>
2026-03-30 13:48:24 +0530

    config: define the network configurations for the network module in order to call the APIs

---
+++ app/src/main/java/com/example/movielog/core/network/NetworkModule.kt
@@ -0,0 +1,35 @@
+package com.example.movielog.core.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object NetworkModule {
+
+    private const val TMDB_BASE_URL = "https://api.themoviedb.org/3/"
+    private const val JIKAN_BASE_URL = "https://api.jikan.moe/v4/"
+
+    // 🔹 Logger (VERY useful for debugging)
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    // 🔹 TMDB Retrofit
+    val tmdbRetrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(TMDB_BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    // 🔹 JIKAN Retrofit
+    val jikanRetrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(JIKAN_BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
