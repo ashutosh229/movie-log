75583bee744ef4ca77c8763affcaa5e1a5dcb8e2
ashutosh229 <akumarjha875@gmail.com>
2026-03-30 20:25:41 +0530

    feat: write the composable for the search screen in the navigation map

--- app/src/main/java/com/example/movielog/core/navigation/AppNavGraph.kt
+++ app/src/main/java/com/example/movielog/core/navigation/AppNavGraph.kt
@@ -1,14 +1,16 @@
 package com.example.movielog.core.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.NavHost;
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.movielog.features.auth.presentation.screen.LoginScreen
 import com.example.movielog.features.auth.presentation.screen.SignupScreen
-import com.example.movielog.features.auth.presentation.viewmodel.AuthViewModel;
+import com.example.movielog.features.auth.presentation.viewmodel.AuthViewModel
 import com.example.movielog.features.library.domain.repository.LibraryRepository
 import com.example.movielog.features.library.presentation.screen.HomeScreen
+import com.example.movielog.features.library.presentation.screen.LibraryScreen
+import com.example.movielog.features.library.presentation.viewmodel.LibraryViewModel
 import com.example.movielog.features.search.presentation.screen.SearchScreen
 import com.example.movielog.features.search.presentation.viewmodel.SearchViewModel
 
@@ -16,7 +18,8 @@ import com.example.movielog.features.search.presentation.viewmodel.SearchViewMod
 fun AppNavGraph(
     authViewModel: AuthViewModel,
     searchViewModel: SearchViewModel,
-    libraryRepository: LibraryRepository
+    libraryRepository: LibraryRepository,
+    libraryViewModel: LibraryViewModel
 ) {
     val navController = rememberNavController()
     val startDestination = if (authViewModel.getCurrentUser() != null) {
@@ -57,12 +60,17 @@ fun AppNavGraph(
                 ) { popUpTo(Routes.HOME) { inclusive = true } }
             }, onNavigateToSearch = {
                 navController.navigate(Routes.SEARCH)
+            }, onNavigateToShow = {
+                navController.navigate(Routes.LIBRARY)
             })
         }
         composable(Routes.SEARCH) {
             SearchScreen(
                 viewModel = searchViewModel, libraryRepository = libraryRepository
             )
+        }
+        composable(Routes.LIBRARY) {
+            LibraryScreen(viewModel = libraryViewModel)
         }
     }
 }
