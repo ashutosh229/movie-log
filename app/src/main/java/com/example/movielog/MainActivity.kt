cdfa282aab206752ee9cc85d01efbc8edb6f59de
ashutosh229 <akumarjha875@gmail.com>
2026-03-30 20:26:21 +0530

    feat: incorporate search screen navigation in the main activity entry point

--- app/src/main/java/com/example/movielog/MainActivity.kt
+++ app/src/main/java/com/example/movielog/MainActivity.kt
@@ -10,6 +10,7 @@ import com.example.movielog.features.auth.data.repository.AuthRepositoryImpl
 import com.example.movielog.features.auth.presentation.viewmodel.AuthViewModel
 import com.example.movielog.features.library.data.remote.LibraryRemoteDataSource
 import com.example.movielog.features.library.data.repository.LibraryRepositoryImpl
+import com.example.movielog.features.library.presentation.viewmodel.LibraryViewModel
 import com.example.movielog.features.search.data.remote.SearchRemoteDataSource
 import com.example.movielog.features.search.data.repository.SearchRepositoryImpl
 import com.example.movielog.features.search.presentation.viewmodel.SearchViewModel
@@ -21,22 +22,27 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 //        TODO: Migration of entities from entry point to navigation graph
+        val authRepository = AuthRepositoryImpl(
+            FirebaseAuthDataSource()
+        )
         val authViewModel = AuthViewModel(
-            AuthRepositoryImpl(
-                FirebaseAuthDataSource()
-            )
+            authRepository
+        )
+        val searchRepository = SearchRepositoryImpl(
+            SearchRemoteDataSource()
         )
         val searchViewModel = SearchViewModel(
-            SearchRepositoryImpl(
-                SearchRemoteDataSource()
-            )
+            searchRepository
         )
         val libraryRepository = LibraryRepositoryImpl(
             LibraryRemoteDataSource()
         )
+        val libraryViewModel = LibraryViewModel(
+            libraryRepository
+        )
         setContent {
             MovieLogTheme {
-                AppNavGraph(authViewModel, searchViewModel, libraryRepository)
+                AppNavGraph(authViewModel, searchViewModel, libraryRepository, libraryViewModel)
             }
         }
     }
