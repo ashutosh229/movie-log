30993c03be7b112e1f5f4cdcb00ec286057d00d8
ashutosh229 <akumarjha875@gmail.com>
2026-03-30 14:19:38 +0530

    chore: add the coil-compose dependency for carrying out async image loading

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,4 +69,5 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("io.coil-kt:coil-compose:2.5.0")
 }
