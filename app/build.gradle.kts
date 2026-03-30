fc3578e5084d33ca42c591def80f17aa1d6113d0
ashutosh229 <akumarjha875@gmail.com>
2026-03-30 11:52:24 +0530

    chore: configure a dependency for the jetpack-compose-navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,4 +64,5 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 }
