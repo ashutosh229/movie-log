23d13fca024ae35e434d6c43f81089e3a4a608cf
ashutosh229 <akumarjha875@gmail.com>
2026-04-02 15:45:47 +0530

    docs: add a TODO for checking the content types from the API later on

--- app/src/main/java/com/example/movielog/features/search/domain/model/ContentType.kt
+++ app/src/main/java/com/example/movielog/features/search/domain/model/ContentType.kt
@@ -1,5 +1,6 @@
 package com.example.movielog.features.search.domain.model
 
+//TODO: need to solve the discripancy in the content type coming from the API
 enum class ContentType {
     MOVIE,
     SERIES,
