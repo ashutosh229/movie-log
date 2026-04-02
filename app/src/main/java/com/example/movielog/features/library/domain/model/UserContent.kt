043c68bc0d3e33f5ae6c6f1ffd123200a68de7d6
ashutosh229 <akumarjha875@gmail.com>
2026-04-02 15:41:18 +0530

    docs: write the todo for checking the mapping status

--- app/src/main/java/com/example/movielog/features/library/domain/model/UserContent.kt
+++ app/src/main/java/com/example/movielog/features/library/domain/model/UserContent.kt
@@ -2,16 +2,17 @@ package com.example.movielog.features.library.domain.model
 
 import com.example.movielog.features.search.domain.model.ContentType
 
+//TODO:
+//firebase structure
+//mapper
+//UPDATE SAVE LOGIC
 data class UserContent(
     val id: String,                 // same as contentId
     val title: String,
     val imageUrl: String?,
     val type: ContentType,
-
     val status: WatchStatus,
-
     val progress: Progress? = null,
-
     val createdAt: Long = System.currentTimeMillis(),
     val updatedAt: Long = System.currentTimeMillis()
 )
