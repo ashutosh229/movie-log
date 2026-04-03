467fc0a450270f06cf5a443078162ddbaacd03c8
ashutosh229 <akumarjha875@gmail.com>
2026-04-04 00:27:15 +0530

    feat: implement the hide and show password functionality in login and signup screen

--- app/src/main/java/com/example/movielog/features/auth/presentation/screen/LoginScreen.kt
+++ app/src/main/java/com/example/movielog/features/auth/presentation/screen/LoginScreen.kt
@@ -8,10 +8,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -27,6 +32,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import com.example.movielog.features.auth.presentation.state.AuthState
 import com.example.movielog.features.auth.presentation.viewmodel.AuthViewModel
@@ -40,6 +46,7 @@ fun LoginScreen(
 
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var passwordVisible by remember { mutableStateOf(false) }
 
     val state = viewModel.authState
 
@@ -104,7 +111,29 @@ fun LoginScreen(
                             onValueChange = { password = it },
                             label = { Text("Password") },
                             singleLine = true,
-                            visualTransformation = PasswordVisualTransformation(),
+                            visualTransformation = if (passwordVisible) {
+                                VisualTransformation.None
+                            } else {
+                                PasswordVisualTransformation()
+                            },
+                            trailingIcon = {
+                                IconButton(onClick = {
+                                    passwordVisible = !passwordVisible
+                                }) {
+                                    Icon(
+                                        imageVector = if (passwordVisible) {
+                                            Icons.Default.VisibilityOff
+                                        } else {
+                                            Icons.Default.Visibility
+                                        },
+                                        contentDescription = if (passwordVisible) {
+                                            "Hide Password"
+                                        } else {
+                                            "Show Password"
+                                        }
+                                    )
+                                }
+                            },
                             modifier = Modifier.fillMaxWidth()
                         )
 
