e84af84597c2101c1183c8edaa3852f192a1e477
ashutosh229 <akumarjha875@gmail.com>
2026-04-03 20:10:08 +0530

    feat: create the navigation graph for the main application

---
+++ app/src/main/java/com/example/movielog/core/navigation/navGraph/MainAppNavGraph.kt
@@ -0,0 +1,62 @@
+package com.example.movielog.core.navigation.navGraph
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.movielog.core.auth.AuthManager
+import com.example.movielog.core.navigation.Routes
+import com.example.movielog.features.library.domain.repository.LibraryRepository
+import com.example.movielog.features.library.presentation.screen.HomeScreen
+import com.example.movielog.features.library.presentation.screen.LibraryScreen
+import com.example.movielog.features.library.presentation.viewmodel.LibraryViewModel
+import com.example.movielog.features.profile.presentation.screen.ProfileScreen
+import com.example.movielog.features.search.presentation.screen.SearchScreen
+import com.example.movielog.features.search.presentation.viewmodel.SearchViewModel
+
+@Composable
+fun MainAppNavGraph(
+    navController: NavHostController,
+    searchViewModel: SearchViewModel,
+    libraryRepository: LibraryRepository,
+    libraryViewModel: LibraryViewModel
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Routes.HOME
+    ) {
+
+        composable(Routes.HOME) {
+            HomeScreen(
+                onLogout = {
+                    AuthManager.logout()
+                },
+                onNavigateToSearch = {
+                    navController.navigate(Routes.SEARCH)
+                },
+                onNavigateToShow = {
+                    navController.navigate(Routes.LIBRARY)
+                }
+            )
+        }
+
+        composable(Routes.SEARCH) {
+            SearchScreen(
+                viewModel = searchViewModel,
+                libraryRepository = libraryRepository
+            )
+        }
+
+        composable(Routes.LIBRARY) {
+            LibraryScreen(viewModel = libraryViewModel)
+        }
+
+        composable(Routes.PROFILE) {
+            ProfileScreen(
+                onLogout = {
+                    AuthManager.logout()
+                }
+            )
+        }
+    }
+}
