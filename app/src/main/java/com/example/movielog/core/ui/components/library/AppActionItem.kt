dce191f5b96c159aa5a9e4dfdc99ccc22e1b76d0
ashutosh229 <akumarjha875@gmail.com>
2026-04-03 13:58:59 +0530

    feat:implement a reusable component for the action buttons in the actions content list

---
+++ app/src/main/java/com/example/movielog/core/ui/components/library/AppActionItem.kt
@@ -0,0 +1,53 @@
+package com.example.movielog.core.ui.components.library
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AppActionItem(
+    icon: ImageVector,
+    text: String,
+    isDestructive: Boolean = false,
+    onClick: () -> Unit
+) {
+    val contentColor = if (isDestructive) {
+        MaterialTheme.colorScheme.error
+    } else {
+        MaterialTheme.colorScheme.onSurface
+    }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClick() }
+            .padding(horizontal = 16.dp, vertical = 14.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Icon(
+            imageVector = icon,
+            contentDescription = text,
+            tint = contentColor
+        )
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        Text(
+            text = text,
+            style = MaterialTheme.typography.bodyLarge,
+            color = contentColor
+        )
+    }
+}
