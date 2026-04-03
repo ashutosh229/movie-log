f9df3c91da2a5e6def128afe507585548a3e5ba1
ashutosh229 <akumarjha875@gmail.com>
2026-04-03 20:40:29 +0530

    feat: implement a main app container, containing both navigation bar and main application

---
+++ app/src/main/java/com/example/movielog/core/navigation/MainAppContainer.kt
@@ -0,0 +1,36 @@
+package com.example.movielog.core.navigation
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.example.movielog.core.navigation.navGraph.MainAppNavGraph
+import com.example.movielog.core.ui.components.common.BottomNavBar
+import com.example.movielog.features.library.domain.repository.LibraryRepository
+import com.example.movielog.features.library.presentation.viewmodel.LibraryViewModel
+import com.example.movielog.features.search.presentation.viewmodel.SearchViewModel
+
+@Composable
+fun MainAppContainer(
+    searchViewModel: SearchViewModel,
+    libraryRepository: LibraryRepository,
+    libraryViewModel: LibraryViewModel
+) {
+    val navController = rememberNavController()
+
+    Scaffold(
+        bottomBar = {
+            BottomNavBar(navController)
+        }
+    ) { innerPadding ->
+
+        MainAppNavGraph(
+            navController = navController,
+            searchViewModel = searchViewModel,
+            libraryRepository = libraryRepository,
+            libraryViewModel = libraryViewModel,
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
