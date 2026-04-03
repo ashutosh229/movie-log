926797094601fe8bdfcfa74799f158ed530eeddd
ashutosh229 <akumarjha875@gmail.com>
2026-04-03 20:10:24 +0530

    feat: create the navigation graph for the whole application

--- app/src/main/java/com/example/movielog/core/navigation/AppNavGraph.kt
+++ app/src/main/java/com/example/movielog/core/navigation/AppNavGraph.kt
@@ -3,19 +3,13 @@ package com.example.movielog.core.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.movielog.core.auth.AuthManager
-import com.example.movielog.features.auth.presentation.screen.LoginScreen
-import com.example.movielog.features.auth.presentation.screen.SignupScreen
+import com.example.movielog.core.navigation.navGraph.AuthNavGraph
+import com.example.movielog.core.navigation.navGraph.MainAppNavGraph
 import com.example.movielog.features.auth.presentation.viewmodel.AuthViewModel
 import com.example.movielog.features.library.domain.repository.LibraryRepository
-import com.example.movielog.features.library.presentation.screen.HomeScreen
-import com.example.movielog.features.library.presentation.screen.LibraryScreen
 import com.example.movielog.features.library.presentation.viewmodel.LibraryViewModel
-import com.example.movielog.features.search.presentation.screen.SearchScreen
 import com.example.movielog.features.search.presentation.viewmodel.SearchViewModel
 
 @Composable
@@ -28,74 +22,20 @@ fun AppNavGraph(
     val navController = rememberNavController()
 
     val authState by AuthManager.authState.collectAsState()
-    key(authState) {
-        val startDestination = if (authState != null) {
-            Routes.HOME
-        } else {
-            Routes.LOGIN
-        }
-        NavHost(
+
+    if (authState == null) {
+        AuthNavGraph(
             navController = navController,
-            startDestination = startDestination
-        ) {
-
-            composable(Routes.LOGIN) {
-                LoginScreen(
-                    viewModel = authViewModel,
-                    onSwitchToSignup = {
-                        navController.navigate(Routes.SIGNUP)
-                    },
-                    onLoginSuccess = {
-                        navController.navigate(Routes.HOME) {
-                            popUpTo(0)
-                        }
-                    }
-                )
+            authViewModel = authViewModel,
+            onAuthSuccess = {
             }
-
-            composable(Routes.SIGNUP) {
-                SignupScreen(
-                    viewModel = authViewModel,
-                    onSwitchToLogin = {
-                        navController.navigate(Routes.LOGIN)
-                    },
-                    onSignupSuccess = {
-                        navController.navigate(Routes.HOME) {
-                            popUpTo(0)
-                        }
-                    }
-                )
-            }
-
-            composable(Routes.HOME) {
-                HomeScreen(
-                    onLogout = {
-                        AuthManager.logout()
-                        // ❌ DO NOT NAVIGATE MANUALLY
-                    },
-                    onNavigateToSearch = {
-                        navController.navigate(Routes.SEARCH)
-                    },
-                    onNavigateToShow = {
-                        navController.navigate(Routes.LIBRARY)
-                    }
-                )
-            }
-
-            composable(Routes.SEARCH) {
-                if (authState == null) return@composable
-
-                SearchScreen(
-                    viewModel = searchViewModel,
-                    libraryRepository = libraryRepository
-                )
-            }
-
-            composable(Routes.LIBRARY) {
-                if (authState == null) return@composable
-
-                LibraryScreen(viewModel = libraryViewModel)
-            }
-        }
+        )
+    } else {
+        MainAppNavGraph(
+            navController = navController,
+            searchViewModel = searchViewModel,
+            libraryRepository = libraryRepository,
+            libraryViewModel = libraryViewModel
+        )
     }
 }
