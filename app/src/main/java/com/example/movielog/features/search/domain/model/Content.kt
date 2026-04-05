58bac159fc0f3aad04ec0409a21eb50c0bf5c351
ashutosh229 <akumarjha875@gmail.com>
2026-04-05 21:23:08 +0530

    feat: redesign the content model to capture more information

--- app/src/main/java/com/example/movielog/features/search/domain/model/Content.kt
+++ app/src/main/java/com/example/movielog/features/search/domain/model/Content.kt
@@ -3,9 +3,11 @@ package com.example.movielog.features.search.domain.model
 data class Content(
     val id: String,
     val title: String,
+//    TODO: for now, nullability handled from UI, later design placeholder
     val imageUrl: String?,
+    val releaseYear: String,
+//  TODO: later, we will se whether to write default values or not
+    val description: String,
+    val language: String,
     val type: ContentType,
-    val totalSeasons: Int? = null,
-    val releaseYear: String? = null,
-    val rating: Double? = null
 )
