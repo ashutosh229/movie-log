de56003ee26e468a8aeb901ee2af59406c2ba86f
ashutosh229 <akumarjha875@gmail.com>
2026-08-17 03:11:00 +0530

    chore: write the API level configuration file

---
+++ app/src/main/java/com/example/movielog/core/config/ApiConfig.kt
@@ -0,0 +1,11 @@
+package com.example.movielog.core.config
+
+object ApiConfig {
+
+    // API base URLs
+    const val TMDB_BASE_URL = "https://api.themoviedb.org/3/"
+    const val JIKAN_BASE_URL = "https://api.jikan.moe/v4/"
+
+    // Image URLs
+    const val TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
+}
